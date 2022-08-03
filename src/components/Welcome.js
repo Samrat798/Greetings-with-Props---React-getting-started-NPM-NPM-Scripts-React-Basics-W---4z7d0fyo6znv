5d1470a6fd812ea65b40857560e964b61d440dff
Samrat798 <71579289+Samrat798@users.noreply.github.com>
2022-08-03 09:50:19 +0530

    aster

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from 'react';
+const Welcome = (props) => {
+  //   let hi = `Hay ${name}`;
+  //   let greeting = `Welcome to Newton School.`;
+  return (
+    <div>
+      <h1>Hay {props.name}</h1>
+      <h2>Welcome to Newton School.</h2>
+    </div>
+  );
+};
+
+export default Welcome;
